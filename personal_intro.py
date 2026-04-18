463a50ca5d8d1b2bdff514542c56377d8cc8043c
Geetanjali Sharma <104669811+Geetanjalishrma@users.noreply.github.com>
2026-04-18 15:06:13 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,6 @@
+name = input("what is your name?")
+age = int(input("what is your age?"))
+hobby = input("what is your favorite hobby?")
+
+print(f"!Welcome {name}!")
+print(f"you are {age} years old and love {hobby}")          
