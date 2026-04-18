816d819f71cc9dd95afb7003aa91f6e76269b8e5
Geetanjali Sharma <104669811+Geetanjalishrma@users.noreply.github.com>
2026-04-18 15:05:11 +0530

    Add files via upload

---
+++ personal_intro_output.py
@@ -0,0 +1,9 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+========== RESTART: C:/Users/ANJALI/OneDrive/Desktop/personal_intro.py =========
+what is your name?geet
+what is your age?20
+what is your favorite hobby?sports
+!Welcome geet!
+you are 20 years old and love sports
